94f8f05de3796399c8043d9701a561155b70bc3a
taybro23 <taybro343@outlook.com>
2021-09-15 19:28:15 +0000

    Added forms, admin and signals.

--- checkout/models.py
+++ checkout/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVER_THRESHOLD:
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PACKAGE / 100
         else:
             self.delivery_cost = 0
