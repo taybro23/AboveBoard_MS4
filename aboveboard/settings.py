11b8b04d4468720769a70f91b2feb7a0b197a06f
taybro23 <taybro343@outlook.com>
2021-09-29 12:42:17 +0000

    Removed secret key and set debug.

--- aboveboard/settings.py
+++ aboveboard/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lo0pqz)o#6=(4gxl(p_k-e!e&vr%%jh@d6&oho^qs(ftqk4&u0'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aboveboard-ms4.herokuapp.com', 'localhost']
 
