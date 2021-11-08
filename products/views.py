d2e95bb5bd67a7c51a614e5d270a17b1427946b8
taybro23 <taybro343@outlook.com>
2021-11-08 12:58:51 +0000

    Reviews to be deleted when product is deleted.

--- products/views.py
+++ products/views.py
@@ -211,6 +211,8 @@ def delete_product(request, product_id):
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
+    reviews = product.reviews.all()
     product.delete()
+    reviews.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
